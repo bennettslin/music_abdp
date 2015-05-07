532992aa72150a161857b2e6b3929cea8072a012
Mistuh_P <mistuhpat@gmail.com>
2015-05-07 10:46:41 -0700

    started user edit and update routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  end
+
   private
 
   def user_params
