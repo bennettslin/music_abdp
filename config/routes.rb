33ef8bce6d84f075da8c94505e5fe3600a26b733
Daniel Smith <smithdp60@gmail.com>
2015-05-07 17:21:57 -0700

    added player and updated styling

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   post 'signup' => 'users#create'
 
   get 'about' => 'site#about'
+  get 'test' => 'test#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
