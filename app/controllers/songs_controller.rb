77c734c04d514d3959ef61e41ac505a1c524a80f
Daniel Smith <smithdp60@gmail.com>
2015-05-08 11:48:53 -0700

    messing with song controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,20 +1,17 @@
 class SongsController < ApplicationController
-  require 'rspotify'
 
-  def artist
+  def related_artist
     artist = RSpotify::Artist.search('Sam Smith')
     #search term variable
     artist.each do |x|
-      @id=x.id
+      @id = x.id
       return @related_artists unless @related_artists.nil?
-      # json = RSpotify.get("artists/7Ln80lUS6He07XvHI8qqHH/related-artists")
-      json = RSpotify.get("artists/#{@id}/related-artists")
-      @related_artists = json['artists'][0,2]
+      data = RSpotify.get("artists/#{@id}/related-artists")
+      @related_artists = data['artists'][0,2]
 
-# render :json => artist
+      # pass id into related artist string
+      render :json => @related_artists
+    end
+  end
 
-# pass id into related artist string
-render :json => @related_artists
-end
-end
 end
