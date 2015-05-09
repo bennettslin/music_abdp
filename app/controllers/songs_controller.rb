2aea56d1dcfbfa07ba6a8832bb9b273a01d7d644
Daniel Smith <smithdp60@gmail.com>
2015-05-08 17:05:56 -0700

    messing with queries

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,23 +1,19 @@
 class SongsController < ApplicationController
 
-  attr_accessor :genre
+  # attr_accessor :genre
 
   def genre
-    genre = RSpotify::Artist.search('genre:Pop')
+    genre = RSpotify::Artist.search('genre:Pop', limit: 25)
     #search term variable
     genre.each do |x|
       @id = x.id
       return @genre unless @genre.nil?
       data = RSpotify.get("artists/#{@id}/related-artists")
-      @genre = data['artists'][0,3]
-
+      @genre = data['artists'][rand(0..24),3]
       # pass id into related artist string
       render :json => @genre
+      # render :plain => @genre
+      # render :json => RSpotify::Artist.search(@genre)
     end
   end
-
-  def artist
-
-  end
-
 end
