997f2e910b77d4dfeab410f0fe588e8271dd3c16
Bennett Lin <bennettslin@gmail.com>
2015-05-11 13:58:18 -0700

    Add blues, pop, and r&b artist lists.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -82,27 +82,42 @@ class SongsController < ApplicationController
   end
 
   def validate_artists
-    genre_objects = []
+    spotify_results = []
+
     genre_artists.each do |object|
-      genre_objects << object[:genre]
-      artist_song_objects = [];
-      artists = object[:artists]
-      artists.each do |artist_string|
+      spotify_artist_objects = [];
+
+      object[:artists].each do |artist_string|
         artist_objects = RSpotify::Artist.search(artist_string, limit: 1)
+
+        spotify_artist_object = {}
         if artist_objects.any?
-          artist_object = artist_objects[0]
-          top_tracks = artist_object.top_tracks(:US)
+
+          spotify_artist_object[:artist_name] = artist_string
+          top_tracks = artist_objects[0].top_tracks(:US)
+
           if top_tracks.any?
-            artist_song_objects << artist_string + " = " + top_tracks[0].name
+            spotify_top_tracks = []
+
+            # get top three tracks, or however many in the array
+            max = [top_tracks.count, 3].min
+            (0...max).each do |i|
+              spotify_top_tracks << top_tracks[i].name
+            end
+
+            spotify_artist_object[:top_tracks] = spotify_top_tracks
           else
-            artist_song_objects << artist_string + " has no top tracks!!!!!!!!!!!!!!!!!!!!"
+            spotify_artist_object[:top_tracks] = "no top tracks!!!!!!!!!!!!!!!!!!!!"
           end
+
         else
-          artist_song_objects << artist_string + " not found!!!!!!!!!!!!!!!!!!!!"
+          spotify_artist_object[:artist_name] = artist_string + " not found!!!!!!!!!!!!!!!!!!!!"
         end
+        spotify_artist_objects << spotify_artist_object
       end
-      genre_objects << artist_song_objects
+      spotify_results << {:genre => object[:genre], :artists => spotify_artist_objects}
     end
-    render :json => genre_objects
+
+    render :json => spotify_results
   end
 end
