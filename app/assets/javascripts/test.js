53a8d39b7300e7922fc15e76aaa729179735a102
Daniel Smith <smithdp60@gmail.com>
2015-05-12 10:57:45 -0700

    score shows on page

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 
+  var count = 0;
 
   $("#q1").click(function(){
+    count += 1;
     $(this).css("background-color", "#00b200").delay(1000);
     $(".incorrect1").css('opacity', '0').delay(1000);
     setTimeout(function() {
-    }, 9000);
+    }, 1000);
     $("#question1").delay(1000).fadeOut("fast", function() {
       $(this).delay(1000).hide();
     });
@@ -15,23 +17,27 @@ $(document).ready(function(){
   });
 
   $(".incorrect1").click(function(){
-    $(".incorrect1").css('opacity', '0').delay(1000);
+    $(this).css("background-color", "#ff0000").delay(1000);
+    setTimeout(function() {
+      $(".incorrect1").css('opacity', '0');
+    }, 1000);
     $("#q1").css("background-color", "#00b200").delay(1000);
     setTimeout(function() {
-    }, 9000);
-    $("#question1").delay(1000).fadeOut("fast", function() {
-      $(this).hide();
-    });
-    $("#question2").delay(400).fadeIn(1000, function() {
-      $(this).show();
-    });
+      $("#question1").delay(1000).fadeOut("fast", function() {
+        $(this).hide();
+      });
+      $("#question2").delay(1000).fadeIn(1000, function() {
+        $(this).show();
+      });
+    }, 1000);
   })
 
   $("#q2").click(function(){
+    count += 1;
     $(this).css("background-color", "#00b200").delay(1000);
     $(".incorrect2").css('opacity', '0').delay(1000);
     setTimeout(function() {
-    }, 9000);
+    }, 1000);
     $("#question2").delay(1000).fadeOut("fast", function() {
       $(this).hide();
     });
@@ -41,40 +47,52 @@ $(document).ready(function(){
   });
 
   $(".incorrect2").click(function(){
-    $(this).css("background-color", "#888888").delay(1000);
-    $("#q2").css("background-color", "#00b200").delay(1000);
-    $(".incorrect2").css('opacity', '0').delay(1000);
+    $(this).css("background-color", "#ff0000").delay(1000);
     setTimeout(function() {
-    }, 9000);
-    $("#question2").delay(1000).fadeOut("fast", function() {
-      $(this).hide();
-    });
-    $("#question3").delay(400).fadeIn(1000, function() {
-      $(this).show();
-    });
+      $(".incorrect2").css('opacity', '0');
+    }, 1000);
+    $("#q2").css("background-color", "#00b200").delay(1000);
+    setTimeout(function() {
+      $("#question2").delay(1000).fadeOut("fast", function() {
+        $(this).hide();
+      });
+      $("#question3").delay(1000).fadeIn(1000, function() {
+        $(this).show();
+      });
+    }, 1000);
   })
 
+
   $("#q3").click(function(){
+    count += 1;
+    console.log(count);
     $(this).css("background-color", "#00b200").delay(1000);
     $(".incorrect3").css('opacity', '0').delay(1000);
     setTimeout(function() {
-    }, 9000);
+    }, 1000);
     $("#question3").delay(700).fadeOut("fast", function() {
       $(this).hide();
     });
     $("#song-cover").delay(800).animate({opacity: 1.0}, 800);
+    $("#results").show()
   });
 
+
   $(".incorrect3").click(function(){
-    $(this).css("background-color", "#888888").delay(1000);
-    $("#q3").css("background-color", "#00b200").delay(1000);
-    $(".incorrect3").css('opacity', '0').delay(1000);
+    console.log(count);
+    $(this).css("background-color", "#ff0000").delay(1000);
     setTimeout(function() {
-    }, 9000);
-    $("#question3").delay(700).fadeOut("fast", function() {
-      $(this).hide();
-    });
-    $("#song-cover").delay(800).animate({opacity: 1.0}, 800);
+      $(".incorrect3").css('opacity', '0');
+    }, 1000);
+    $("#q3").css("background-color", "#00b200").delay(1000);
+    setTimeout(function() {
+      $("#question3").delay(500).fadeOut("fast", function() {
+        $(this).hide();
+      });
+    }, 800);
+    $("#song-cover").delay(1200).animate({opacity: 1.0}, 800);
+    $("#score").text(count);
+    $("#results").show()
   });
 
 });
