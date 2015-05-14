cb396441891dc03d85cff12216d2ed978e388fdd
Bennett Lin <bennettslin@gmail.com>
2015-05-14 13:59:57 -0700

    Committing recent changes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,130 +5,142 @@ class UsersController < ApplicationController
   require 'net/https'
   respond_to :html, :json
 
-  def friends
-    if @current_user && @current_user.provider == 'facebook'
+  def stats
+    if @current_user
 
-    # FIXME: this is not DRY!
+      # FIXME: this is not DRY!
 
-    # create genre hashes
-    genre_hashes = empty_genre_hashes
+      # create genre hashes
+      genre_hashes = empty_genre_hashes
 
-    # create friends hashes
-    friends_array = facebook_friends_array @current_user
-    friends_array << @current_user
+      # create friends hash if facebook user, otherwise just user
+      friends_array = nil
+      user_hashes = nil
+      if @current_user.provider == 'facebook'
 
-    user_hashes = friends_array.map do |user|
-      {
-        user_id: user.id,
-        genre_hashes: empty_genre_hashes
-      }
-    end
+        # create friends hashes
+        friends_array = facebook_friends_array @current_user
+        friends_array << @current_user
 
-    # add quiz data (genre, user, id) to hashes
-    Quiz.all.map do |quiz|
-
-      # get genre from quiz song
-      song = Song.find(quiz.song_id)
-      genre = Genre.find(song.genre_id)
-      genre_hash = genre_hashes[genre.value]
-
-      # get user_hash of quiz's user
-      user = User.find(quiz.user_id)
-      if friends_array.include? user
-        user_hash = user_hash_from_user_hashes user_hashes, user
-        user_genre_hash = user_hash[:genre_hashes][genre.value]
-
-        # get true scores for each question from binary score
-        quiz_score_array = score_array_from_binary_score quiz.result
-
-        # add true scores to genre and user hashes
-        (0...quiz_score_array.count).each do |i|
-          genre_hash[:total_scores_array][i] += quiz_score_array[i]
-          user_genre_hash[:total_scores_array][i] += quiz_score_array[i]
+        user_hashes = friends_array.map do |user|
+          {
+            user_id: user.id,
+            genre_hashes: empty_genre_hashes
+          }
         end
-
-        # add quiz to total
-        genre_hash[:total_quizzes] += 1
-        user_genre_hash[:total_quizzes] += 1
+      else
+        friends_array = [@current_user]
+        user_hashes = [{
+          user_id: @current_user.id,
+          genre_hashes: empty_genre_hashes
+        }]
       end
-    end
 
-    # remember percentage for each genre
-    add_percentages_to_genre_hashes genre_hashes
+      # add quiz data (genre, user, id) to hashes
+      Quiz.all.map do |quiz|
 
-    # remember ratings for each user
-    user_hashes.each do |user_hash|
-      add_ratings_to_genre_hashes user_hash[:genre_hashes]
-    end
+        # get genre from quiz song
+        song = Song.find(quiz.song_id)
+        genre = Genre.find(song.genre_id)
+        genre_hash = genre_hashes[genre.value]
 
-    all_highest_ratings = []
-    (0...genre_hashes.count).each do |i| # i is number of genres
-      genre_highest_ratings = []
+        # get user_hash of quiz's user
+        user = User.find(quiz.user_id)
+        if friends_array.include? user
+          user_hash = user_hash_from_user_hashes user_hashes, user
+          user_genre_hash = user_hash[:genre_hashes][genre.value]
 
-      (0...3).each do |j| # j is number of questions
-        question_highest_ratings = []
+          # get true scores for each question from binary score
+          quiz_score_array = score_array_from_binary_score quiz.result
 
-        (0...5).each do |k| # k is number of leaders
-          question_highest_ratings << {user_id:0, rating:0}
+          # add true scores to genre and user hashes
+          (0...quiz_score_array.count).each do |i|
+            genre_hash[:total_scores_array][i] += quiz_score_array[i]
+            user_genre_hash[:total_scores_array][i] += quiz_score_array[i]
+          end
+
+          # add quiz to total
+          genre_hash[:total_quizzes] += 1
+          user_genre_hash[:total_quizzes] += 1
         end
-        genre_highest_ratings << question_highest_ratings
       end
-      all_highest_ratings << genre_highest_ratings
-    end
 
-    (0...user_hashes.count).each do |i| # i is number of users
-      user_hash = user_hashes[i]
-      (0...genre_hashes.count).each do |j| # j is number of genres
-        user_genre_hash = user_hash[:genre_hashes][j]
-        (0...user_genre_hash[:ratings_array].count).each do |k| # k is number of questions
+      # remember percentage for each genre
+      add_percentages_to_genre_hashes genre_hashes
 
-          compared_id = user_hash[:user_id]
-          compared_rating = user_genre_hash[:ratings_array][k]
+      # remember ratings for each user
+      user_hashes.each do |user_hash|
+        add_ratings_to_genre_hashes user_hash[:genre_hashes]
+      end
 
-          (0...5).each do |l| # l is number of leaders
-            if compared_rating > all_highest_ratings[j][k][l][:rating]
+      all_highest_ratings = []
+      (0...genre_hashes.count).each do |i| # i is number of genres
+        genre_highest_ratings = []
 
-              next_compared_id = all_highest_ratings[j][k][l][:user_id]
-              next_compared_rating = all_highest_ratings[j][k][l][:rating]
+        (0...3).each do |j| # j is number of questions
+          question_highest_ratings = []
 
-              all_highest_ratings[j][k][l][:user_id] = compared_id
-              all_highest_ratings[j][k][l][:rating] = compared_rating
+          (0...5).each do |k| # k is number of leaders
+            question_highest_ratings << {user_id:0, rating:0}
+          end
+          genre_highest_ratings << question_highest_ratings
+        end
+        all_highest_ratings << genre_highest_ratings
+      end
 
-              compared_id = next_compared_id
-              compared_rating = next_compared_rating
+      (0...user_hashes.count).each do |i| # i is number of users
+        user_hash = user_hashes[i]
+        (0...genre_hashes.count).each do |j| # j is number of genres
+          user_genre_hash = user_hash[:genre_hashes][j]
+          (0...user_genre_hash[:ratings_array].count).each do |k| # k is number of questions
+
+            compared_id = user_hash[:user_id]
+            compared_rating = user_genre_hash[:ratings_array][k]
+
+            (0...5).each do |l| # l is number of leaders
+              if compared_rating > all_highest_ratings[j][k][l][:rating]
+
+                next_compared_id = all_highest_ratings[j][k][l][:user_id]
+                next_compared_rating = all_highest_ratings[j][k][l][:rating]
+
+                all_highest_ratings[j][k][l][:user_id] = compared_id
+                all_highest_ratings[j][k][l][:rating] = compared_rating
+
+                compared_id = next_compared_id
+                compared_rating = next_compared_rating
+              end
             end
           end
         end
       end
-    end
 
-    all_highest_ratings.each do |highest_ratings|
-      highest_ratings.each do |genre_ratings|
-        genre_ratings.each do |question_ratings|
-          user_id = question_ratings[:user_id]
-          if user_id != 0
-            user = User.find(user_id)
-            question_ratings[:user_name] = user.first_name + " " + user.last_name[0, 1] + "."
-            question_ratings[:user] = user
+      all_highest_ratings.each do |highest_ratings|
+        highest_ratings.each do |genre_ratings|
+          genre_ratings.each do |question_ratings|
+            user_id = question_ratings[:user_id]
+            if user_id != 0
+              user = User.find(user_id)
+              question_ratings[:user_name] = user.first_name + " " + user.last_name[0, 1] + "."
+              question_ratings[:user] = user
 
-          else
-            question_ratings[:user_name] = "No leader"
+            else
+              question_ratings[:user_name] = "No leader"
+            end
           end
         end
       end
-    end
 
-    # if current user, include data for current user in view
-    if @current_user
-      user_hashes.each do |user_hash|
-        if @current_user.id == user_hash[:user_id]
-          @user_hash = user_hash
+      # if current user, include data for current user in view
+      if @current_user
+        user_hashes.each do |user_hash|
+          if @current_user.id == user_hash[:user_id]
+            @user_hash = user_hash
+          end
         end
       end
-    end
 
-    @genre_hashes = genre_hashes
-    @all_highest_ratings = all_highest_ratings
+      @genre_hashes = genre_hashes
+      @all_highest_ratings = all_highest_ratings
 
     else
       redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
   post 'signup' => 'users#create'
-  get 'users/:id/friends' => 'users#friends'
+  get 'users/:id/stats' => 'users#stats'
 
   get 'about' => 'site#about'
   get 'leaderboard' => 'site#leaderboard'
