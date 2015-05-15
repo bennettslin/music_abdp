22dd325c5ede1f34c174e50b1bef7f7a844c0f6d
Bennett Lin <bennettslin@gmail.com>
2015-05-15 10:05:54 -0700

    precompile assets.

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module MusicAbdp
 
     config.active_record.raise_in_transactional_callbacks = true
     config.exceptions_app = self.routes
+    config.assets.precompile += [
+      'red@2x.png', 'red.css'
+    ]
   end
 end
